2f5aa3657379ee15f8ab23213d7ff18c82879602
Mohd Othman <myphpmaster@gmail.com>
2021-03-24 22:37:04 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Testing Azure Server
         </h1>
 
         <p className={styles.description}>

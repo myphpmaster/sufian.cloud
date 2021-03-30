a6fd0288f20a3d1ca56e2c173c6dc6cfda625799
Mohd Othman <myphpmaster@gmail.com>
2021-03-30 10:35:40 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,12 +8,17 @@ module.exports = {
     };
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-
+    
+      config.node = {
+        fs: 'empty'
+      }
+      
       config.plugins.push(new webpack.ProvidePlugin({
           $: 'jquery',
           jQuery: 'jquery',
           'window.jQuery': 'jquery'
       }))
+
       
       // Important: return the modified config
       return config;

b008827568ade5b46f028885db9f0cb9e51d2126
Mohd Othman <myphpmaster@gmail.com>
2021-04-03 13:48:20 +0800

    Update AdminContain.js

--- components/AdminContain.js
+++ components/AdminContain.js
@@ -53,7 +53,7 @@ export const Contain = () => {
     const isReachingEnd =
         isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE);
     const isRefreshing = isValidating && data && data.length === size;
-*/
+
 
     const { data: survey } = useSWR(() => '/api/charts/', fetcher)
     const arr = survey ? [].concat(...survey) : [];
@@ -144,7 +144,27 @@ export const Contain = () => {
         let identity = resultx[i]['identity'];
     }
 
+
+    
     /* Calculate Server running days */
+    
+    
+    const { data } = useSWR(() => '/api/submissions?limit=1&page=1', fetcher)
+    const datas = data ? [].concat(...data) : [];
+
+    const results = [];
+    datas.forEach(function(value, index, array) {
+        // The callback is executed for each element in the array.
+        // `value` is the element itself (equivalent to `array[index]`)
+        // `index` will be the index of the element in the array
+        // `array` is a reference to the array itself (i.e. `datas` in this case)
+        results.push(value.data);
+    }); 
+    
+    const { data: survey } = useSWR(() => '/api/charts/', fetcher)
+    const arr = survey ? [].concat(...survey) : [];
+
+
     // To set two dates to two variables
     const date_start = new Date('03/25/2021');
     const date_now = new Date();
@@ -286,38 +306,126 @@ export const Contain = () => {
                     
                     <div className="bg-white border rounded shadow">
                         <div className="border-b p-3">
-                            <h5 className="font-bold uppercase text-gray-600 text-center">Table</h5>
+                            <h5 className="font-bold uppercase text-gray-600 text-center">Latest Entry</h5>
                         </div>
                         <div className="p-5">
-                            <table className="w-full p-5 text-gray-700">
-                                <thead>
-                                    <tr>
-                                        <th className="text-left text-blue-900">Name</th>
-                                        <th className="text-left text-blue-900">Side</th>
-                                        <th className="text-left text-blue-900">Role</th>
-                                    </tr>
-                                </thead>
 
-                                <tbody>
-                                    <tr>
-                                        <td>Obi Wan Kenobi</td>
-                                        <td>Light</td>
-                                        <td>Jedi</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Greedo</td>
-                                        <td>South</td>
-                                        <td>Scumbag</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Darth Vader</td>
-                                        <td>Dark</td>
-                                        <td>Sith</td>
-                                    </tr>
-                                </tbody>
-                            </table>
 
-                            <p className="py-2"><a href="#">See More issues...</a></p>
+                        { results.map( (val, index) => (
+                            <div key={index} className="pb-10 border-gray-400 border mx-4">
+                                <dl>
+                                <div className="text-center bg-gray-200 px-4 py-5 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-black">
+                                    General
+                                    </dt>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                    Age
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {val.age} years
+                                    </dd>
+                                </div>
+                                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                    Gender
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {val.gender}
+                                    </dd>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                    Highest Education
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {val.education}
+                                    </dd>
+                                </div>
+                                <div className="bg-gray-200 text-center px-4 py-5 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-black">
+                                    Building
+                                    </dt>
+                                </div>
+                                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                    State
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {val.state}
+                                    </dd>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                    Building Category
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {val.buildingCategory}
+                                    </dd>
+                                </div>
+                                <div className="bg-gray-200 text-center px-4 py-5 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-black">
+                                    Thermal
+                                    </dt>
+                                </div>
+                                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                    Temperature in your work space
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
+                                        <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                        <div className="w-0 flex-1 flex items-center">
+
+                                            <span className="ml-2 flex-1 w-0 truncate">
+                                            Morning (08:00 - 11:00)
+                                            </span>
+                                        </div>
+                                        <div className="ml-4 flex-shrink-0">
+                                            {val.tempCommon.morning}
+                                        </div>
+                                        </li>
+                                        <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                            <div className="w-0 flex-1 flex items-center">
+
+                                                <span className="ml-2 flex-1 w-0 truncate">
+                                                Noon (11:00 - 13:00)
+                                                </span>
+                                            </div>
+                                            <div className="ml-4 flex-shrink-0">
+                                                {val.tempCommon.noon}
+                                            </div>
+                                        </li>
+                                        <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                            <div className="w-0 flex-1 flex items-center">
+
+                                                <span className="ml-2 flex-1 w-0 truncate">
+                                                Afternoon (13:00 - 17:00)
+                                                </span>
+                                            </div>
+                                            <div className="ml-4 flex-shrink-0">
+                                                {val.tempCommon.afternoon}
+                                            </div>
+                                        </li>
+                                        <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                            <div className="w-0 flex-1 flex items-center">
+
+                                                <span className="ml-2 flex-1 w-0 truncate">
+                                                Evening (17:00 - 21:00)
+                                                </span>
+                                            </div>
+                                            <div className="ml-4 flex-shrink-0">
+                                                {val.tempCommon.evening}
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    </dd>
+                                </div>
+                                </dl>
+                            </div>        
+                        ))}
+                        
 
                         </div>
                     </div>

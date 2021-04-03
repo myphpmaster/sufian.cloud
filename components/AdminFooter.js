8ff3c683d537418a12a0c27b972a8e5683cb5243
Mohd Othman <myphpmaster@gmail.com>
2021-04-03 14:08:16 +0800

    Update AdminFooter.js

--- components/AdminFooter.js
+++ components/AdminFooter.js
@@ -4,52 +4,70 @@ import { useState } from 'react';
 
 export const Footer = () => {
 
-    const [active, setActive] = useState({
-        a: false,
-        b: false,
-        c: false,
-        d: false,
-        e: false,
-        f: false,
-        g: false
-    });
-
-    const handleClick = (e) => {
-        setActive(active =>  ({ 
-            ...active, 
-            [e.target.name]: !active[e.target.name] 
-        }) );
-    };
-    
+    const menus = [
+        {
+           "id":"menu-home",
+           "title":"Home",
+           "url":"/"
+        },
+        {
+           "id":"menu-caprover",
+           "title":"Caprover",
+           "target": "_blank",
+           "url":"https://captain.app.sufian.cloud/"
+         },
+        {
+           "id":"menu-form",
+           "title":"Form Dashboard",
+           "url":"https://survey.app.sufian.cloud/#/",
+           "target": "_blank"
+         },
+         {
+           "id":"menu-code",
+           "title":"Source Code",
+           "url":"https://github.com/myphpmaster/sufian.cloud",
+           "target": "_blank"
+         },
+         {
+           "id":"menu-azure",
+           "title":"Azure Portal",
+           "url":"https://portal.azure.com/",
+           "target": "_blank"
+         }
+     ];
+         
   return (
     <>
 
 <footer className="bg-white border-t border-gray-400 shadow">
-        <div className="container max-w-md mx-auto flex py-8">
+        <div className="container max-w-12xl mx-auto flex py-8">
 
             <div className="w-full mx-auto flex flex-wrap">
                 <div className="flex w-full md:w-1/2 ">
                     <div className="px-8">
                         <h3 className="font-bold font-bold text-gray-900">About</h3>
                         <p className="py-4 text-gray-600 text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
+                            Microsoft Azure Hackathon Project.
                         </p>
                     </div>
                 </div>
 
                 <div className="flex w-full md:w-1/2">
                     <div className="px-8">
-                        <h3 className="font-bold font-bold text-gray-900">Social</h3>
+                        <h3 className="font-bold font-bold text-gray-900">Links</h3>
                         <ul className="list-reset items-center text-sm pt-3">
-                            <li>
-                                <a className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">Add social link</a>
+                            
+                        { menus.map( (menu, index) => ( 
+                            <li className="inline-block pr-5">
+                                <Link key={index} href={menu.url}>
+                                    <a id={menu.id}  target={menu.target ? menu.target : `_self`}
+                                            className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1">
+                                        {menu.title}
+                                    </a>
+                                </Link>
                             </li>
-                            <li>
-                                <a className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">Add social link</a>
-                            </li>
-                            <li>
-                                <a className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">Add social link</a>
-                            </li>
+                        ))}
+
                         </ul>
                     </div>
                 </div>

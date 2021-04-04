f0e73b6248922a08bcffafa84b036f7cfb8686d2
Mohd Othman <myphpmaster@gmail.com>
2021-04-04 23:18:51 +0800

    Update AdminLogin.js

--- components/AdminLogin.js
+++ components/AdminLogin.js
@@ -7,7 +7,9 @@ export default function Login () {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <img className="mx-auto h-12 w-auto" src="/logo.png" alt="sufian.cloud" />
+          <a href="/">
+            <img className="mx-auto h-12 w-auto" src="/logo.png" alt="sufian.cloud" />
+          </a>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Access Denied! You must be signed in to view this page
           </h2>

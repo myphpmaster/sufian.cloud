adb7647031027cd9b99ea81cef83078625a6dd3e
Mohd Othman <myphpmaster@gmail.com>
2021-04-11 03:42:48 +0800

    Update latestSubmission.js

--- components/admin/latestSubmission.js
+++ components/admin/latestSubmission.js
@@ -43,7 +43,7 @@ export const Table = () => {
                                             </dt>
                                         </div>
                                         
-                                        { renderData(section.components,val) }
+                                        { ReactDOMServer.render(renderData(section.components,val)) }
 
                                     </>
 

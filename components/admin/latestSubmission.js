e0f7978c902eb9bdc36ce802f1db62dc7f05ee43
Mohd Othman <myphpmaster@gmail.com>
2021-04-11 03:23:58 +0800

    Update latestSubmission.js

--- components/admin/latestSubmission.js
+++ components/admin/latestSubmission.js
@@ -289,15 +289,15 @@ function renderData(params, variable) {
 
         if( typeof variable[params[i].key] !== 'object'  ){
 
-            html += '' +
+            html += `
                 <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt className="text-sm font-medium text-gray-500">
-                        {params[i].label}
+                        ${params[i].label}
                     </dt>
                     <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                        {variable[params[i].key]}
+                        ${variable[params[i].key]}
                     </dd>
-                </div>;
+                </div>`
             
 
         }

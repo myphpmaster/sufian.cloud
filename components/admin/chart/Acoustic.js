bf4ee15decc2dad5c135c3e902512c551ff45974
Mohd Othman <myphpmaster@gmail.com>
2021-04-14 14:30:21 +0800

    Update admin pages

--- components/admin/chart/Acoustic.js
+++ components/admin/chart/Acoustic.js
@@ -7,58 +7,34 @@ export const Chart = () => {
     const { data: schem } = useSWR(() => '/api/label', fetcher)
     const schems = schem ? [].concat(...schem) : [];
 
+    const charts = []
+
     for (let i = 0; i < schems.length; i++) {
         
-        if(validType(schems[i].type)) {
+        if(schems[i].type == 'panel') {
 
             let obj = schems[i].components
-            console.log(JSON.stringify(obj))
 
                 for (let j = 0; j < obj.length; j++) {
 
-                    // console.log('obj[j].key =>' + obj[j].key)
+                    // console.log('obj[j] =>' + JSON.stringify(obj[j]))
 
-                    if (obj[j].key) {
-
+                    if (validType(obj[j].key)) {
+                        charts.push(obj[j])
                     }
                 }
-
         }
     }
 
     return (
 
-    <section>
+    <section className="container flex">
 
-                    <div className="w-full md:w-1/2 p-3">
-                        
-                        <div className="bg-white border rounded shadow">
-                            <div className="border-b p-3">
-                                <h5 className="font-bold uppercase text-gray-600 text-center">Background noise in your work area</h5>
-                            </div>
-                            <div className="p-5">
-                                <div className="relative" style={{width: '100%', height: '500px'}}>
-                                    <iframe className="absolute inset-0 w-full h-full" src="/chart/horizontal/backgroundNoise/" frameBorder="0" />
-                                </div>
-                            </div>
-                        </div>
-                        
-                    </div>
+        {charts.map( (section, key) => (      
+            
+            renderCharts(section, key)
 
-                    <div className="w-full md:w-1/2 p-3">
-                        
-                        <div className="bg-white border rounded shadow">
-                            <div className="border-b p-3">
-                                <h5 className="font-bold uppercase text-gray-600 text-center">Any noise from mechanical or electrical equipment?</h5>
-                            </div>
-                            <div className="p-5">
-                                <div className="relative" style={{width: '100%', height: '500px'}}>
-                                    <iframe className="absolute inset-0 w-full h-full" src="/chart/pie/MEnoise" frameBorder="0" />
-                                </div>
-                            </div>
-                        </div>
-                        
-                    </div>
+        ))}    
 
     </section>
 
@@ -74,4 +50,28 @@ function validType(type){
         'survey',
     ]
     return types.includes(type) ? true : false
+}
+
+function renderCharts(data, id){
+
+    var chartType = type; 
+
+    return (
+        
+        <div key={id} className="w-full md:w-1/2 p-3">
+                        
+            <div className="bg-white border rounded shadow">
+                <div className="border-b p-3">
+                    <h5 className="font-bold uppercase text-gray-600 text-center">{data.label}</h5>
+                </div>
+                <div className="p-5">
+                    <div className="relative" style={{width: '100%', height: '500px'}}>
+                        <iframe className="absolute inset-0 w-full h-full" src={`/chart/horizontal/${data.key}/`} frameBorder="0" />
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+    )
 }

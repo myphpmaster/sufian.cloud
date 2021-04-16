9376f813576ebaeb0cd6c352267d45a3adfe3b64
Mohd Othman <myphpmaster@gmail.com>
2021-04-17 04:03:51 +0800

    Update latestSubmission.js

--- components/admin/latestSubmission.js
+++ components/admin/latestSubmission.js
@@ -84,7 +84,7 @@ export const Table = () => {
                                                     </dt>
                                                 </div>
                                                 {renders.filter(el => el.panel.id == key).map( (comp, num) => ( 
-                                                    renderNew(comp, val, num)
+                                                    renderData(comp, val, num)
                                                 ))} 
                                             </div>
                                         ))}    
@@ -98,109 +98,7 @@ export const Table = () => {
     );
 };
 
-function renderData(params, variable, schema, id) {
-
-    var key = variable[params.key]  // variables[parameters.key]
-    var val = params.key            // parameters.key
-    var rawData = realValue(key, val, schema)
-    var table = ["bg-gray-50", "bg-white"]
-    var tableClass = (id % 2 == 0) ? table[0] : table[1]
-
-    if(params.input) {
-
-        if( typeof key !== 'object' ){
-
-            return (
-                <div key={id} className={`${tableClass} px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6`}>
-                    <dt className="text-sm font-medium text-gray-500">
-                        {params.label}
-                    </dt>
-                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                        {rawData}
-                    </dd>
-                </div>
-            );
-
-        } else {
-
-            const obj = variable[params.key];
-            const objNames = Object.keys(obj);
-            const objVal = Object.values(obj);
-            var newArr = []
-
-            for (let k in obj){
-
-                newArr[k] = obj[k]
-
-            }
-
-            // console.log('objNames => ' + JSON.stringify(objNames) )
-            // console.log('objVal => ' + JSON.stringify(objVal) )
-            // console.log('newArr => ' + JSON.stringify(newArr) )
-
-            return (
-                <div key={id} className={`${tableClass} px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6`}>
-                    <dt className="text-sm font-medium text-gray-500">
-                        {params.label}
-                    </dt>
-                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                        <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
-                            
-                            { objNames.map( (com, num)=> (    
-                                                    
-                                renderSubdata(com, newArr, params.key, schema, num) 
-                                
-                            ))}
-
-                        </ul>
-                    </dd>
-                </div>
-            );
-
-        }
-    } else {
-
-        if(params.type == 'columns') {
-
-            let column = params.columns
-            for (let i = 0; i < column.length; i++) {
-
-                let comp = column[i].components
-                
-                //console.log('comp[' + i + ']=>' + JSON.stringify(comp))
-
-                for (let j = 0; j < comp.length; j++) {
-
-                    if( typeof variable[comp[j].key] !== 'object' ){
-
-                        key = variable[comp[j].key]
-                        val = comp[j].key
-                        rawData = realValue(key, val, schema)
-                        //console.log('key[' + j + ']=>' + JSON.stringify(key))
-                        //console.log('val[' + j + ']=>' + JSON.stringify(val))
-                        //console.log('var[' + j + ']=>' + JSON.stringify(variable))
-
-                        renderSubdata(key, variable, val, schema, id)
-
-                            return (
-                                <div key={id} className={`${tableClass} px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6`}>
-                                    <dt className="text-sm font-medium text-gray-500">
-                                        {comp[j].label}
-                                    </dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                        {rawData}
-                                    </dd>
-                                </div>
-                            );
-                        
-                    } 
-                }
-            }
-        }
-    }
-}
-
-function renderNew(comp, val, id) {
+function renderData(comp, val, id) {
 
     var table = ["bg-gray-50", "bg-white"]
     var tableClass = (id % 2 == 0) ? table[0] : table[1]
@@ -287,101 +185,6 @@ function renderSurvey(question, values, value, num) {
 
     )
 
-}
-
-function renderSubdata(subparams, array, key, schema, id) {
-   
-    let rawTitle = realValue(subparams, key, schema, true)
-    let rawData = realValue(array[subparams], key, schema) 
-    var table = ["bg-gray-50", "bg-white"]
-    var tableClass = (id % 2 == 0) ? table[0] : table[1]
-    
-    return (
-        
-        <li key={id} className={`${tableClass}pl-3 pr-4 py-3 flex items-center justify-between text-sm`}>
-            <div className="w-0 flex-1 flex items-center">
-
-                <span className="ml-2 flex-1 w-0 truncate">
-                    {rawTitle}
-                </span>
-            </div>
-            <div className="ml-4 flex-shrink-0">
-                    {rawData}
-            </div>
-        </li>
-
-    )
-
-}
-
-function realValue(key, value, schema, title=false){
-
-    let rawData = key
-    let rawKey = value
-
-    for (let i = 0; i < schema.length; i++) {
-
-        let obj = schema[i].components
-
-        for (let j = 0; j < obj.length; j++) {
-
-            // console.log('obj[j].key =>' + obj[j].key)
-
-            var values
-
-            if (obj[j].type == 'columns') {
-                let col = obj[j].columns
-                for (let k = 0; k < col.length; k++) {
-
-                    let objCol = col[k].components
-                    // console.log('objCol[' + k + ']=>' + JSON.stringify(objCol))
-                    // console.log('key[' + k + ']=>' + JSON.stringify(key))
-
-                    for (let l = 0; l < objCol.length; l++) {
-                        if( objCol[l].key==rawKey){
-                            values = objCol[l]
-                            break
-                        }
-                    }
-
-                }
-
-            } else {
-                
-                values = obj[j]
-            }
-            
-            if (rawKey == values.key) {
-
-
-                // For dropdown select input
-                if( values.hasOwnProperty('data') ){
-                    values = values.data
-                }
-
-                // radio input directly have this property
-                if( values.hasOwnProperty('values') ){
-
-                    let realVal = values.values
-
-                    if(title){
-                        realVal = values.questions
-                    }
-
-                    if(typeof realVal === 'object'){
-                        for (let k = 0; k < realVal.length; k++) {
-
-                            if(rawData == realVal[k].value || rawData === realVal[k].value ) {
-                                rawData = realVal[k].label
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return rawData
 }
 
 function filterProps(objects={},props=[],inputs={}){

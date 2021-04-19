32219f440b45ec0ffe12b638cec9116dc10f53ff
Mohd Othman <myphpmaster@gmail.com>
2021-04-19 18:21:31 +0800

    Update [[...slug]].js

--- pages/admin/[[...slug]].js
+++ pages/admin/[[...slug]].js
@@ -107,6 +107,10 @@ export default function Admin() {
 		
 		<div className="container w-full mx-auto pt-20">
 			<div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
+				
+				{ (!page) && <>
+				<Notice />
+        		</>}
 
 				<RespondData />
 
@@ -125,11 +129,6 @@ export default function Admin() {
 					
 					</nav>
 				</div>    
-					
-				{ (!page) && <>
-				<Notice />
-        		</>}
-
 
 				{ (!schem) && <>
 				
